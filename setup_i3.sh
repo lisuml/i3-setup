d9d06e54a3d428f0b8e1857d88a75d749884b641
Michal Lisowski <lisu87@gmail.com>
2016-09-12 16:42:14 +0100

    Add initial Xresources file stolen from Manjaro

--- setup_i3.sh
+++ setup_i3.sh
@@ -2,7 +2,7 @@
 
 set -e #abort on error
 
-sudo pacman -Sy --noconfirm i3 dash feh i3lock rofi compton imagemagick scrot pulseaudio-ctl gsimplecal
+sudo pacman -Sy --noconfirm i3 dash feh i3lock rofi compton imagemagick scrot pulseaudio-ctl gsimplecal rxvt-unicode
 yaourt -S --noconfirm ttf-font-awesome i3blocks-git
 ruby -e "$(curl -fsSL https://raw.github.com/supermarin/YosemiteSanFranciscoFont/master/install)"
 
@@ -18,3 +18,4 @@ cp ./i3blocks.conf $HOME/.config/i3blocks/config
 cp ./config $HOME/.config/i3/config
 cp ./i3lock-wrapper $HOME/.config/i3/i3lock-wrapper
 cp ./wallpaper/ $HOME/.wallpaper/*
+cp ./Xresources $HOME/.Xresources
