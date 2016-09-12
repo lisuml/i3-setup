26fc0c66ed49eab0eed699113a026f1cb6165435
Michal Lisowski <lisu87@gmail.com>
2016-09-12 22:25:31 +0100

    Add file manager (pcmanfm)

--- setup_i3.sh
+++ setup_i3.sh
@@ -3,7 +3,7 @@
 #set -e #abort on error
 
 sudo pacman -Sy --noconfirm i3 dash feh i3lock rofi compton imagemagick scrot pulseaudio-ctl gsimplecal rxvt-unicode acpi
-yaourt -S --noconfirm ttf-font-awesome i3blocks
+yaourt -S --noconfirm ttf-font-awesome i3blocks pcmanfm
 ruby -e "$(curl -fsSL https://raw.github.com/supermarin/YosemiteSanFranciscoFont/master/install)"
 
 sudo mkdir -p  /usr/lib/i3blocks
