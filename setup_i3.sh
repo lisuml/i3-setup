e549e8c90724c3c089bb5d2e8b586bf7d8967f83
Michal Lisowski <lisu87@gmail.com>
2016-09-12 19:16:34 +0100

    Move i3blocks related things to separated dir

--- setup_i3.sh
+++ setup_i3.sh
@@ -7,14 +7,14 @@ yaourt -S --noconfirm ttf-font-awesome i3blocks-git
 ruby -e "$(curl -fsSL https://raw.github.com/supermarin/YosemiteSanFranciscoFont/master/install)"
 
 sudo mkdir -p  /usr/lib/i3blocks
-sudo cp -r ./scripts/* /usr/lib/i3blocks/
+sudo cp -r ./i3blocks/scripts/* /usr/lib/i3blocks/
 
 mkdir -p $HOME/.config/i3blocks
 mkdir -p $HOME/.config/i3
 mkdir -p $HOME/.wallpaper
 mkdir -p $HOME/screenshots
 
-cp ./i3blocks.conf $HOME/.config/i3blocks/config
+cp ./i3blocks/i3blocks.conf $HOME/.config/i3blocks/config
 cp ./config $HOME/.config/i3/config
 cp ./i3lock-wrapper $HOME/.config/i3/i3lock-wrapper
 cp ./wallpaper/ $HOME/.wallpaper/*
